cb826ea0ce2a00750c1307bc319ca1ef8d59d6b7
anasmuostafa <anasmuostafa@gmail.com>
2024-01-04 21:00:57 +0200

    adjust styling

--- frontend/src/components/dashboard/Dashboard_Login.jsx
+++ frontend/src/components/dashboard/Dashboard_Login.jsx
@@ -3,65 +3,73 @@ import React from "react";
 export default function Dashboard_Login() {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6 col-md-4 offset-md-4">
-            <div className="login-section">
-              <div className="account-wall">
-                <form className="form-signin">
-                  <div className="input-group mb-2 mr-sm-2">
-                    <div className="input-group-prepend">
-                      <div className="input-group-text">
-                        <span className="material-icons"> perm_identity </span>
-                      </div>
-                    </div>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="inlineFormInputGroupUsername2"
-                      placeholder="Username/Email"
-                    />
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="login-section">
+          <div className="account-wall">
+            <form className="form-signin">
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span className="material-icons"> perm_identity </span>
                   </div>
-                  <div className="input-group mb-2 mr-sm-2">
-                    <div className="input-group-prepend">
-                      <div className="input-group-text">
-                        <span className="material-icons"> lock </span>
-                      </div>
-                    </div>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="inlineFormInputGroupUsername2"
-                      placeholder="password"
-                    />
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inlineFormInputGroupUsername2"
+                  placeholder="Username/Email"
+                />
+              </div>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span className="material-icons"> lock </span>
                   </div>
-                  <label className="checkbox pull-left">
-                    <input type="checkbox" value="remember-me" />
-                    Remember me
-                  </label>
-                  <a href="#" className="pull-right need-help">
-                    <button
-                      className="btn btn-lg btn-danger btn-block"
-                      type="submit"
-                    >
-                      Log in
-                    </button>
-                  </a>
-                  <span className="clearfix"></span>
-                </form>
-              </div>
-              <div className="login-footer">
-                <div className="pull-left">
-                  <a href="#" className="text-center new-account">
-                    Forget Password?
-                  </a>
                 </div>
-                <div className="pull-right">
-                  <a href="#" className="text-center new-account">
-                    Get New Account <b>Sign up</b>
-                  </a>
-                </div>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inlineFormInputGroupUsername2"
+                  placeholder="password"
+                />
               </div>
+              <label className="checkbox pull-left d-flex align-items-center justify-content-around">
+                Remember me
+                <input
+                  type="checkbox"
+                  value="remember-me"
+                  style={{ width: "fit-content" }}
+                />
+              </label>
+              <a href="#" className="pull-right need-help">
+                <button
+                  className="btn btn-lg btn-danger btn-block"
+                  type="submit"
+                >
+                  Log in
+                </button>
+              </a>
+              <span className="clearfix"></span>
+            </form>
+          </div>
+          <div className="login-footer">
+            <div className="pull-left">
+              <a href="#" className="text-center new-account">
+                Forget Password?
+              </a>
+            </div>
+            <div className="pull-right">
+              <a href="#" className="text-center new-account">
+                Get New Account <b>Sign up</b>
+              </a>
             </div>
           </div>
         </div>
