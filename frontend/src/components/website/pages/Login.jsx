a4d514d917be550014aa59758fe85765206be78e
anasmuostafa <anasmuostafa@gmail.com>
2024-01-05 13:05:59 +0200

    adding login and register

---
+++ frontend/src/components/website/pages/Login.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="login-section">
+          <div className="account-wall">
+            <form className="form-signin">
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span className="material-icons"> perm_identity </span>
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inlineFormInputGroupUsername1"
+                  placeholder="Username/Email"
+                />
+              </div>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span className="material-icons"> lock </span>
+                  </div>
+                </div>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inlineFormInputGroupUsername2"
+                  placeholder="password"
+                />
+              </div>
+              {<label className="checkbox pull-left d-flex align-items-center justify-content-around">
+                Remember me
+                <input
+                  type="checkbox"
+                  value="remember-me"
+                  style={{ width: "fit-content" }}
+                />
+              </label>}
+              <a href="#" className="pull-right need-help">
+                <button
+                  className="btn btn-lg btn-danger btn-block"
+                  type="submit"
+                >
+                  Log in
+                </button>
+              </a>
+              <span className="clearfix"></span>
+            </form>
+          </div>
+          <div className="login-footer">
+            <div className="pull-left">
+              <a href="#" className="text-center new-account">
+                Forget Password?
+              </a>
+            </div>
+            <div className="pull-right">
+              <a
+                href="#"
+                className="text-center new-account"
+              >
+                Get New Account <b>Sign up</b>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/website/pages/Register.jsx
@@ -0,0 +1,159 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function Register(props) {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Do something with the captured input values (e.g., send them to the server)
+    try {
+      const result = await fetch("http://localhost:5001/api/users/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: formData.name,
+          email: formData.email,
+          password: formData.password,
+          type: "customer",
+        }),
+      });
+      console.log({ result });
+      let res = await result.json();
+      console.log({ res });
+      if (!result.ok) {
+        const errorMessage = res.message;
+        console.log(errorMessage);
+        throw new Error(errorMessage);
+      }
+      console.log("Register Data:", {
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        type: "customer",
+      });
+      toast.success("User Registered Successfully");
+      setTimeout(() => {
+        props.setShownPage(5);
+      }, 1000);
+    } catch (errorData) {
+      const errorMessage =
+        errorData?.message || "An error occurred during registration";
+      toast.error("Error Registering: " + errorMessage);
+    }
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className="register-section">
+          <div className="account-wall">
+            <form className="form-signin" onSubmit={handleSubmit}>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span className="material-icons"> perm_identity </span>
+                  </div>
+                </div>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span class="material-symbols-outlined">mail</span>
+                  </div>
+                </div>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="input-group mb-2 mr-sm-2">
+                <div className="input-group-prepend">
+                  <div className="input-group-text">
+                    <span className="material-icons"> lock </span>
+                  </div>
+                </div>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <label className="checkbox pull-left d-flex align-items-center justify-content-around">
+                Remember me
+                <input
+                  type="checkbox"
+                  value="remember-me"
+                  style={{ width: "fit-content" }}
+                />
+              </label>
+              <button
+                onClick={handleSubmit}
+                className="btn btn-lg btn-danger btn-block"
+                type="submit"
+              >
+                Register
+              </button>
+            </form>
+          </div>
+          <div className="register-footer">
+            <div className="pull-left">
+              <a href="#" className="text-center new-account">
+                Forget Password?
+              </a>
+            </div>
+            <div className="pull-right">
+              <a
+                onClick={() => {
+                  props.setShownPage(5);
+                }}
+                href="#"
+                className="text-center new-account"
+              >
+                Already Have Account <b>Sign in</b>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
