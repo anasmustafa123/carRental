d4bd317964ab1b825572d37d65b87602aa1ab526
anasmuostafa <anasmuostafa@gmail.com>
2024-01-08 06:17:32 +0200

    go for production

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ import carsRoutes from "./routes/carRoutes.js";
 import reservationRoutes from "./routes/reservationRoutes.js";
 import officeRoutes from "./routes/officeRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import cors from "cors"; 
+import cors from "cors";
+import path from "path";
 //adding the .env const to process.env
 dotenv.config();
 
-console.log(process.env)
-console.log(process.env.MYSQL_PASSWORD)
+console.log(process.env);
+console.log(process.env.MYSQL_PASSWORD);
 let port = process.env.PORT || 5200;
 const app = express();
 
@@ -22,29 +23,39 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// 
-app.use(  
-  cors({ 
+//
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
 //app.options("/api/users/auth", cors());
 
-app.get("/", (req, res) => {
-  console.log("anas");
-  res.send("server is ready");
-});
 
 app.use("/api/users", userRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/cars", carsRoutes);
 app.use("/api/reservations", reservationRoutes);
 app.use("/api/offices", officeRoutes);
- 
+
+if (process.env.NODE_ENV == "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+app.get("/", (req, res) => {
+  console.log("anas");
+  res.send("server is ready");
+});
 app.use(notFound);
 app.use(errorHandler);
 
+
+ 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
