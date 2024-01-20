191cfc7914594bf1b5ee45aae61aee550a46b7a7
anasmuostafa <anasmuostafa@gmail.com>
2024-01-20 18:59:12 +0200

    set secure to true

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,8 @@ const generateToken = (res, userEmail, type) => {
   });
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
+    //secure: process.env.NODE_ENV !== "development",
     sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
